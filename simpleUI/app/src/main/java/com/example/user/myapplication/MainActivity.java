4593ab4fb5cf9bf05a48054f319c8bc5135fef5b
chialingliao <green730304@yahoo.com.tw>
2016-04-21 19:57:25 +0800

    add buttom and editText

--- simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;//宣告
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);//去找VIEW ,須轉型態
+
+
+    }
+
+    public void click(View view){//BTN 改值 須設定ONCLICK
+        textView.setText("Android Class");
     }
 }
