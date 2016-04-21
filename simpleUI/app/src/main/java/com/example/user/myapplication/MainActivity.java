d0d4774b2ca1c47395a7fe7e2b92c4b6f48ffacf
chialingliao <green730304@yahoo.com.tw>
2016-04-21 21:15:53 +0800

    implement setOnCheckedChangeListener

--- simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -6,13 +6,15 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;//宣告
     EditText editText;
-
+    RadioGroup radioGroup;
+    String sex = "Male";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);//去找VIEW ,須轉型態
         editText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+
         editText.setOnKeyListener(new View.OnKeyListener() {//ENTER 等同BTN效果
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {//虛擬鍵盤有作用　
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE){//須將SINGLELINE打勾才能有反應
+                if (actionId == EditorInfo.IME_ACTION_DONE) {//須將SINGLELINE打勾才能有反應
                     click(v);
                     return true;
                 }
@@ -41,10 +46,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.maleRadioButton){
+                    sex = "Male";
+                }else if(checkedId == R.id.femaleRadioButton){
+                    sex = "Female";
+                }
+            }
+        });
+
     }
 
     public void click(View view) {//BTN 改值 須設定ONCLICK
         String text = editText.getText().toString(); //把畫面資料抓出來
+        text = text + " sex: " + sex;//資料 + radio值
         textView.setText(text);
         editText.setText("");//資料抓完清空
     }
