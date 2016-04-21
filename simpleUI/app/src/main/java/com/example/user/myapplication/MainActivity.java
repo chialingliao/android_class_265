c815c12afd61171948739c9bced1241cf54279f3
chialingliao <green730304@yahoo.com.tw>
2016-04-21 20:56:24 +0800

    implement Listener and add radioButton

--- simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,21 +2,52 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;//宣告
+    EditText editText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);//去找VIEW ,須轉型態
-
+        textView = (TextView) findViewById(R.id.textView);//去找VIEW ,須轉型態
+        editText = (EditText) findViewById(R.id.editText);
+        editText.setOnKeyListener(new View.OnKeyListener() {//ENTER 等同BTN效果
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    click(v);
+                    return true;//攔截ENTER
+                }
+                return false;
+            }
+        });
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {//虛擬鍵盤有作用　
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){//須將SINGLELINE打勾才能有反應
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
-    public void click(View view){//BTN 改值 須設定ONCLICK
-        textView.setText("Android Class");
+    public void click(View view) {//BTN 改值 須設定ONCLICK
+        String text = editText.getText().toString(); //把畫面資料抓出來
+        textView.setText(text);
+        editText.setText("");//資料抓完清空
     }
+
+
 }
