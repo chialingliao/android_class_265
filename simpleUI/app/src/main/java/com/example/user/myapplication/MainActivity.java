04c74a7ecb6c3e9516848d8e964b850b47ef1cb7
chialingliao <green730304@yahoo.com.tw>
2016-05-09 21:16:06 +0800

    hide imageView

--- simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -156,6 +157,17 @@ public class MainActivity extends AppCompatActivity {
 //                drinkName = radioButton.getText().toString();
 //            }
 //        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    photoImageView.setVisibility(View.GONE);
+                }else{
+                    photoImageView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {//點擊listView 的資訊
             @Override
